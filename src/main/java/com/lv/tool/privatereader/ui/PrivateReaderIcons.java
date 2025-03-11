ee120634d770fb5e3e6f77e766546a56c59c31c1
J1N- <825333220@qq.com>
2025-03-11 13:53:53 +0800

    feat: 添加自定义工具窗口图标，替换默认菜单图标

---
+++ src/main/java/com/lv/tool/privatereader/ui/PrivateReaderIcons.java
@@ -0,0 +1,15 @@
+package com.lv.tool.privatereader.ui;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * 插件图标定义类
+ */
+public class PrivateReaderIcons {
+    /**
+     * 工具窗口图标
+     */
+    public static final Icon TOOL_WINDOW = IconLoader.getIcon("/icons/toolWindowIcon.svg", PrivateReaderIcons.class);
+} 
